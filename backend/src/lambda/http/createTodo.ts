fe902f693ed78b1e07035f2be6496d6591890b1f
Felicia Ebikon <feliciaebikon@gmail.com>
2022-08-07 01:05:14 +0100

    initial commit

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,26 @@
+import {APIGatewayProxyHandler, APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { createTodo } from '../../helpers/todos'
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    // TODO: Implement creating a new TODO item
+    console.log("Processing Event ", event);
+    const authorization = event.headers.Authorization;
+    const split = authorization.split(' ');
+    const jwtToken = split[1];
+
+    const newTodo: CreateTodoRequest = JSON.parse(event.body);
+    const toDoItem = await createTodo(newTodo, jwtToken);
+
+    return {
+        statusCode: 201,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+            "item": toDoItem
+        }),
+    }
+};
+
